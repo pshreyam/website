1c095ed3ea1a255a43a66cfdd60944ce6386dd3a
Shreyam Pokharel <pshreyam@gmail.com>
2022-02-25 21:09:09 +0545

    Update interface

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,71 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Typewriter from 'typewriter-effect'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+import Typewriter from "typewriter-effect";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>Shreyam Pokharel</title>
-        <meta name="description" content="Python Programmer and Linux Enthusiast" />
+        <meta
+          name="description"
+          content="Python Programmer and Linux Enthusiast"
+        />
       </Head>
 
+      <nav className={styles.navbar}>
+        <div className={styles.brand}>
+          <Link href="/">
+            <Image
+              alt="SP Logo"
+              className={styles.brand__logo}
+              src="/logo.png"
+              width={40}
+              height={40}
+            />
+          </Link>
+        </div>
+        <ul className={styles.nav__links}>
+          <li>
+            <Link href="#">Home</Link>
+          </li>
+          <li>
+            <Link href="#blogs">Blogs</Link>
+          </li>
+        </ul>
+      </nav>
+
       <main className={styles.main}>
-        <h4 className={styles.title}>
-          About Me
-        </h4>
-        <Image alt="Shreyam Pokharel" className={styles.profile__image} src="https://gitlab.com/uploads/-/system/user/avatar/7811853/avatar.png" width={200} height={200} />
-      </main>
-
-      <section className={styles.content}>
-        <p className={styles.description}>
-          Hello, this is
-           <Typewriter
-            options={{loop:true, cursor: "_"}}
-            onInit={(typewriter) => {
-              typewriter.typeString('<b>Shreyam Pokharel.</b>')
-                .pauseFor(1000)
-                .deleteAll()
-                .start();
-            }}
+        <section className={styles.header}>
+          <Image
+            alt="Shreyam Pokharel"
+            className={styles.profile__image}
+            src="https://gitlab.com/uploads/-/system/user/avatar/7811853/avatar.png"
+            width={200}
+            height={200}
           />
-          I am currently pursuing Computer Engineering at Kathmandu University.
-        </p>
+        </section>
 
-        <p>
+        <section className={styles.content}>
+          <p className={styles.description}>
+            Hello, this is
+            <Typewriter
+              options={{ loop: true, cursor: "_" }}
+              onInit={(typewriter) => {
+                typewriter
+                  .typeString("<b>Shreyam Pokharel.</b>")
+                  .pauseFor(1000)
+                  .deleteAll()
+                  .start();
+              }}
+            />
+            I am currently pursuing Computer Engineering at Kathmandu
+            University.
+          </p>
+
+          <p>
             <h4 className={styles.skills__header}>My Skills:</h4>
             <ul>
               <li>Python3</li>
@@ -42,32 +74,56 @@ export default function Home() {
               <li>Flask</li>
               <li>PyQt5</li>
             </ul>
-        </p>
-        <p className={styles.connect}>
-          You may connect with me using:
-        </p>
-        <div className={styles.social__links}>
-          <span className={styles.logo}>
-              <a target="_blank" rel="noopener noreferrer" href="https://github.com/pshreyam">
-                    <Image src="/logo/github.svg" alt="Github Logo" width={72} height={16} />
+          </p>
+          <p className={styles.connect}>You may connect with me using:</p>
+          <div className={styles.social__links}>
+            <span className={styles.logo}>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://github.com/pshreyam"
+              >
+                <Image
+                  src="/assets/github.svg"
+                  alt="Github Logo"
+                  width={72}
+                  height={16}
+                />
               </a>
-          </span>
-          <span className={styles.logo}>
-            <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/shreyam-pokharel-62334221a/">
-                  <Image src="/logo/linked-in.svg" alt="LinkedIn Logo" width={72} height={16} />
-            </a>
-          </span>
-          <span className={styles.logo}>
-              <a target="_blank" rel="noopener noreferrer" href="mailto:pshreyam@gmail.com">
-                    <Image src="/logo/email.svg" alt="Email Logo" width={72} height={16} />
+            </span>
+            <span className={styles.logo}>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.linkedin.com/in/shreyam-pokharel-62334221a/"
+              >
+                <Image
+                  src="/assets/linked-in.svg"
+                  alt="LinkedIn Logo"
+                  width={72}
+                  height={16}
+                />
               </a>
-          </span>
-        </div>
-      </section>
+            </span>
+            <span className={styles.logo}>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="mailto:pshreyam@gmail.com"
+              >
+                <Image
+                  src="/assets/email.svg"
+                  alt="Email Logo"
+                  width={72}
+                  height={16}
+                />
+              </a>
+            </span>
+          </div>
+        </section>
+      </main>
 
-      <footer className={styles.footer}>
-        &copy; Shreyam Pokharel (2022)
-      </footer>
+      <footer className={styles.footer}>&copy; Shreyam Pokharel (2022)</footer>
     </div>
-  )
+  );
 }
